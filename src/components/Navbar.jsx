a1931b7d0088dd2e9774eabdc4d652ce7ec1dd20
Shahbaz <shahbazrao445@gmail.com>
2025-09-26 23:53:06 +0500

    Today i am start working on Swiggy clone app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+     <header className='p-3 shadow-x1'>
+      <div className="max-w-[1200px] mx-auto border-red-500">
+        <div className="w-[100px] border border-blue-500">
+          <img src="public/images/Swiggy-logo.webp" alt="" className='w-full'/>
+        </div>
+      </div>
+     </header>
+    </div>
+  )
+}
+
+export default Navbar
